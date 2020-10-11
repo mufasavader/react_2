16808a40b1cb72dec7b1a5c9be9ca86e1ee15209
Mathews Maloba <mathewsmaloba88@gmail.com>
2020-10-11 13:36:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,46 +5,65 @@ import ReactDOM from 'react-dom'
 import './index.css'
 //setup vars
 
+const books=[
+  
+  {
 
-const firstBook={
-  image:"https://images-na.ssl-images-amazon.com/images/I/91BQbqTW6oL._AC_UL200_SR200,200_.jpg",
+  id:1,
+  img:"https://images-na.ssl-images-amazon.com/images/I/71rZvKObxUL._AC_UL200_SR200,200_.jpg",
+  title:"Spooky Pookie (Little Pookie)",
+  author:"Sandra Boynton"
+
+},
+
+
+{
+  id:2,
+  img:"https://images-na.ssl-images-amazon.com/images/I/91BQbqTW6oL._AC_UL200_SR200,200_.jpg",
   title:"Follow the Money: The Shocking Deep State Connections of the Anti-Trump Cabal",
   author:"Dan Bongino"
 
 }
-
-const secondBook={
-  image:"https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg",
+,
+{
+  id:3,
+  img:"https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg",
   title:"Room on the Broom ",
   author:"Julia Donaldson "
 
 }
 
+];
 
 
 function Bestsellerlist() {
   return (
     <section className="booklist">
-     
-     <Book 
-     img={firstBook.image} 
-     title={firstBook.title}
-      author={firstBook.author}/>
-     
-     <Book 
-     img={secondBook.image} 
-     title={secondBook.title} 
-     author={secondBook.author}/>
-     
+      {
+        books.map(
+        (book)=>{
+          //const {img,title,author} =book;
+          return (
+            <Book key= {book.id} book={book}></Book>
+
+          );
+
+
+        }
+          
+
+
+        )
+      }
     </section>
       
     
   )
 }
 
-const Book=({img,title,author})=>{
+const Book=(props)=>{
   
-  //const {img,title,author}=props
+const {img,title,author}=props.book;
   
   return <article className='book'>
     <img src= {img}alt='book'/>
